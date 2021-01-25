abb3af869e505753f079eefa5e52e7643ff626a0
polinayermolenko <polina.shvajukova@gmail.com>
2021-01-25 17:37:11 +0300

    Environment settings

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './App.module.scss';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <header className={classes.AppHeader}>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
